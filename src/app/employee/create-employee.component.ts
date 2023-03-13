69ab0eaec4f5f1b8ad030723ba7af33d11115feb
Sagar Chopra <schopra@firemate.com>
2023-03-13 16:59:49 +0530

    Validation messages in component

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -10,6 +10,38 @@ export class CreateEmployeeComponent implements OnInit {
   employeeForm !: FormGroup;
   fullNameLength: number = 0;
 
+  // This object will hold the messages to be displayed to the user
+// Notice, each key in this object has the same name as the
+// corresponding form control
+formErrors: any = {
+  'fullName': '',
+  'email': '',
+  'skillName': '',
+  'experienceInYears': '',
+  'proficiency': ''
+};
+
+// This object contains all the validation messages for this form
+validationMessages: any = {
+  'fullName': {
+    'required': 'Full Name is required.',
+    'minlength': 'Full Name must be greater than 2 characters.',
+    'maxlength': 'Full Name must be less than 10 characters.'
+  },
+  'email': {
+    'required': 'Email is required.'
+  },
+  'skillName': {
+    'required': 'Skill Name is required.',
+  },
+  'experienceInYears': {
+    'required': 'Experience is required.',
+  },
+  'proficiency': {
+    'required': 'Proficiency is required.',
+  },
+};
+
   constructor(private _fb:FormBuilder){ }
 
   ngOnInit(): void {
@@ -29,9 +61,9 @@ export class CreateEmployeeComponent implements OnInit {
       email: [''],
 
       skills: this._fb.group({
-        skillName: [''],
-        experienceInYears: [''],
-        proficiency: ['beginner']
+        skillName: ['', Validators.required],
+        experienceInYears: ['', Validators.required],
+        proficiency: ['beginner', Validators.required]
       })
     });
 
@@ -42,15 +74,51 @@ export class CreateEmployeeComponent implements OnInit {
     console.log(this.employeeForm.value);
   }
 
-  onLoadDataClick(): void{
-    this.employeeForm.setValue({
-      fullName: 'John',
-      email: 'john@john.com',
-      skills:{
-        skillName: 'C#',
-        experienceInYears: 5,
-        proficiency: 'beginner'
+  // onLoadDataClick(): void{
+  //   this.employeeForm.setValue({
+  //     fullName: 'John',
+  //     email: 'john@john.com',
+  //     skills:{
+  //       skillName: 'C#',
+  //       experienceInYears: 5,
+  //       proficiency: 'beginner'
+  //     }
+  //   })
+  // }
+
+  logValidationErrors(group: FormGroup): void {
+    // Loop through each control key in the FormGroup
+    Object.keys(group.controls).forEach((key: string) => {
+      // Get the control. The control can be a nested form group
+      const abstractControl = group.get(key);
+      // If the control is nested form group, recursively call
+      // this same method
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+        // If the control is a FormControl
+      } else {
+        // Clear the existing validation errors
+        this.formErrors[key] = '';
+        if (abstractControl && !abstractControl.valid) {
+          // Get all the validation messages of the form control
+          // that has failed the validation
+          const messages = this.validationMessages[key];
+          // Find which validation has failed. For example required,
+          // minlength or maxlength. Store that error message in the
+          // formErrors object. The UI will bind to this object to
+          // display the validation errors
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
       }
-    })
+    });
+  }
+  
+  onLoadDataClick(): void {
+    this.logValidationErrors(this.employeeForm);
+    console.log(this.formErrors);
   }
 }
