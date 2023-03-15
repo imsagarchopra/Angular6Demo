9fafaf466d5aad0544192120858a641c4a8eaf51
Sagar Chopra <schopra@firemate.com>
2023-03-15 17:13:35 +0530

    Custom Preloading Strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CustomPreloadingService } from './custom-preloading.service';
 import { HomeComponent } from './home.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
@@ -7,13 +8,13 @@ const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
    // redirect to the home route if the client side route path is empty
    { path: '', redirectTo: '/home', pathMatch: 'full' },
-   {path: 'employees', loadChildren: ()=>import('./employee/employee.module').then(mod=>mod.EmployeeModule)},
+   {path: 'employees', data: {preload: true}, loadChildren: ()=>import('./employee/employee.module').then(mod=>mod.EmployeeModule)},
    // wild card route
    { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes,{preloadingStrategy: PreloadAllModules})],
+  imports: [RouterModule.forRoot(appRoutes,{preloadingStrategy: CustomPreloadingService})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
